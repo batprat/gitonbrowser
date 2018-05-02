599cf27d8f715f28a93a57e15ddffa29ffb54a6c
pratyush <pratyushtamhankar@gmail.com>
2018-05-02 23:37:59 +0200

    Add loaders to all git operations.

--- public/js/app/app.module.js
+++ public/js/app/app.module.js
@@ -13,3 +13,49 @@ webgitApp.controller('AppController', ['$scope', '$window', '$rootScope', functi
     $window.onfocus = onFocus;
     $window.onblur = onBlur;
 }]);
+
+webgitApp.factory('loaderInterceptor', [function() {
+    var requestCount = 0;
+    var $loader = null;
+    var showLoader = function() {
+        $loader = $loader || $('#loader-modal');
+        if(!$loader || $loader.length == 0) {
+            return;
+        }
+        $loader.removeClass('loader-hidden');
+    };
+
+    var hideLoader = function() {
+        $loader = $loader || $('#loader-modal');
+        if(!$loader || $loader.length == 0) {
+            return;
+        }
+        $loader.addClass('loader-hidden');
+    };
+    return {
+        request: function(config) {
+            requestCount++;
+            console.log('a request is made.');
+            showLoader();
+            return config;
+        },
+        response: function(response) {
+            requestCount--;
+            if(requestCount <= 0) {     // shouldn't go less than but still..
+                requestCount = 0;
+                hideLoader();
+            }
+            console.log('a response has arrived.');
+            return response;
+        },
+
+        responseError: function(rejection) {
+            console.log('an error has arrived as response');
+            return rejection;
+        }
+    };
+}]);
+
+webgitApp.config(['$httpProvider', function($httpProvider) {  
+    $httpProvider.interceptors.push('loaderInterceptor');
+}]);
