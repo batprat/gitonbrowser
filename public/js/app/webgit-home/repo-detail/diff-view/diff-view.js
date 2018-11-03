d3f2a3a3e85bf3ad32b5c79069cd1c4f49dadb67
Pratyush <pratyushtamhankar@gmail.com>
2018-11-03 15:46:54 +0100

    Put diff in a separate component. This will be used in the future.

---
+++ public/js/app/webgit-home/repo-detail/diff-view/diff-view.js
@@ -0,0 +1,11 @@
+(function() {
+    angular.module('RepoDetailModule').component('diffView', {
+        templateUrl: '/js/app/webgit-home/repo-detail/diff-view/diff-view.html',
+        bindings: {
+            safeDiff: '<'
+        },
+        controller: [function() {
+            
+        }]
+    });
+})();
